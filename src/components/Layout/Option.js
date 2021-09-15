8b2382cfdb7d5da828d07e59f4dae17bc8206d2c
praveen y <saipraveeny.8@gmail.com>
2021-09-15 12:01:57 +0530

    added icons and fixed the footer

--- src/components/Layout/Option.js
+++ src/components/Layout/Option.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./Option.module.css";
+import homelogo from "../../assets/home.png";
+import orderlogo from "../../assets/order.png";
+import paymentlogo from "../../assets/payment.png";
+import contactlogo from "../../assets/contact.png";
 const Option = () => {
   return (
     // <div className={classes.container}>
@@ -9,13 +13,29 @@ const Option = () => {
       ☰
       <div className={classes.dropdown_content}>
         <li>
-          <Link to="/">home</Link>
+          <Link to="/">
+            <img src={homelogo} alt="homelogo" />
+            {"  "}
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/orders">My orders</Link>
+          <Link to="/orders">
+            <img src={orderlogo} alt="homelogo" />
+            {"  "}My orders
+          </Link>
         </li>
         <li>
-          <Link to="/payments">payment</Link>
+          <Link to="/payments">
+            <img src={paymentlogo} alt="homelogo" />
+            {"  "}Payment
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact">
+            <img src={contactlogo} alt="homelogo" />
+            {"  "}Contact
+          </Link>
         </li>
       </div>
     </div>
